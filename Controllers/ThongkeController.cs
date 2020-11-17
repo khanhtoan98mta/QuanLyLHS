7291ada78d72b9867dfaa95d03838af5a58a3fae
KhanhToan <KhanhToan@DESKTOP-C6JGQK7>
2020-11-17 09:05:33 +0700

    Thống kê tốt nghiệp

--- Controllers/ThongkeController.cs
+++ Controllers/ThongkeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using QLDHS.Models.Entity;
 namespace QLDHS.Controllers
 {
     public class ThongkeController : Controller
@@ -17,11 +17,47 @@ namespace QLDHS.Controllers
         public ActionResult DSThongkeNomon()
         {
 
+            
+
             return View();
         }
 
         public ActionResult DSThongkeKQHT()
         {
+            return View();
+        }
+        public ActionResult ThongKe_LHS_TotNghiep()
+        {
+            List<string> nam = new List<string>();
+            List<int> xs = new List<int>();
+            List<int> xs_ = new List<int>();
+            List<int> gioi = new List<int>();
+            List<int> kha = new List<int>();
+            List<int> tb = new List<int>();
+            List<int> khac = new List<int>();
+
+
+            List<ThongKe_TotNghiep_Year> ThongKeTN = new LUUHS().Database.SqlQuery<ThongKe_TotNghiep_Year>("exec dbo.ThongKe_LHS_TotNghiep").ToList();
+            for (int i = 0; i < ThongKeTN.Count; i++)
+            {
+
+                nam.Add(ThongKeTN[i].Nam.ToString());
+                xs.Add(ThongKeTN[i].Xuatsac);
+                xs_.Add(ThongKeTN[i].Xuatsac_);
+                gioi.Add(ThongKeTN[i].Gioi);
+                kha.Add(ThongKeTN[i].Kha);
+                tb.Add(ThongKeTN[i].TrungBinh);
+                khac.Add(ThongKeTN[i].Khac);
+
+
+            }
+            ViewBag.nam = nam;
+            ViewBag.xs = xs;
+            ViewBag.xs_ = xs_;
+            ViewBag.gioi = gioi;
+            ViewBag.kha = kha;
+            ViewBag.tb = tb;
+            ViewBag.khac = khac;
             return View();
         }
     }
---
+++ Models/Entity/ThongKe_TotNghiep_Year.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLDHS.Models.Entity
+{
+    public class ThongKe_TotNghiep_Year
+    {
+        public int Nam { get; set; }
+        public int Xuatsac_ { get; set; }
+        public int Xuatsac { get; set; }
+        public int Gioi { get; set; }
+        public int Kha { get; set; }
+        public int TrungBinh { get; set; }
+        public int Khac { get; set; }
+
+        public ThongKe_TotNghiep_Year()
+        {
+            
+        }
+        public ThongKe_TotNghiep_Year(int nam, int xuatsac_, int xuatsac, int gioi, int kha, int trungBinh, int khac)
+        {
+            Nam = nam;
+            Xuatsac_ = xuatsac_;
+            Xuatsac = xuatsac;
+            Gioi = gioi;
+            Kha = kha;
+            TrungBinh = trungBinh;
+            Khac = khac;
+        }
+
+    }
+}
